6e9e41f0459abdc8c993321d78f2d0304c78ddf9
samuelBarreto <samukacfc1@gmail.com>
2025-11-03 15:24:55 -0300

    ajsute docker

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human! <3',
+        'message': 'Você está indo muito bem, <3',
         'deployed_on': 'kubernetes'
     })
 
