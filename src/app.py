b504273f50787276cac5754afda249472f0c1e86
samuelBarreto <samukacfc1@gmail.com>
2025-11-03 16:05:40 -0300

    update de imagem e projeto

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'Você está indo muito bem, <3',
+        'message': 'Voce esta indo muito bem, <3',
         'deployed_on': 'kubernetes'
     })
 
